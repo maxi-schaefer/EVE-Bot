01365f4c66b1aa1ba0c5b1b0e3ccef49596e79dd
BuildTools <schaefer.maxi09101@gmail.com>
2022-08-17 17:00:53 +0200

    Updated some Stuff

--- src/types/events/client/guildCreate.js
+++ src/types/events/client/guildCreate.js
@@ -2,23 +2,13 @@ const { Client } = require('discord.js');
 const mongoose = require('mongoose');
 
 module.exports = {
-    name: "ready",
+    name: "guildCreate",
     rest: false,
     once: false,
     /**
      * @param { Client } client
      */
     async execute(client) {
-        /* Connect to the database */
-        const database = client.config.database;
-        if(!database) return console.log('Please add a Database!');
-
-        mongoose.connect(database, {}).then(() => {
-            console.log(`Connected successfully to the Database!`);
-        })
-
-        console.log(`Logged in as ${client.user.tag}!`);
-
         updateActivity(client, client.config.activityInterval);
     }
 }
@@ -44,4 +34,4 @@ module.exports = {
       const status = activities[Math.floor(Math.random() * activities.length)]
       client.user.setActivity(status.replace("{servercount}", servercount))
     }, interval*1000)
-}
+  }
